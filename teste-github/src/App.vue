df26442085a2ee9adba2bc6e87a9be935242378c
Bryan Lucas da Conceição Afinovicz <bryanafinovicz706@gmail.com>
2026-05-23 12:42:08 -0300

    botão pra tentar reatividade

--- teste-github/src/App.vue
+++ teste-github/src/App.vue
@@ -1,7 +1,14 @@
-<script setup></script>
+<script setup>
+ import { ref } from 'vue'
+
+ const count = ref(0)
+</script>
+
 
 <template>
   <h1>Teste de github pages</h1>
+  <p>Contador: {{ count }}</p>
+  <button @click="count++">Incrementar</button>
 </template>
 
 <style scoped></style>
